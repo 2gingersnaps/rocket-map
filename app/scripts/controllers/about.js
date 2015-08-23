687babc225a499ee554e13e66be937bda938803a
2gingersnaps <robbikenney@gmail.com>
2015-08-23 17:21:30 -0400

    initialized new Grunt/Yeoman project

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name rocketMapApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the rocketMapApp
+ */
+angular.module('rocketMapApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name rocketMapApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the rocketMapApp
+ */
+angular.module('rocketMapApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
